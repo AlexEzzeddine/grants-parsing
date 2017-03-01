3992767c33ba8e71b0306fd851f9d24bd9316976
ohoncharuk <alex@honcharuk.me>
2017-03-01 23:31:14 +0200

    mongo

--- board_api/board_api.py
+++ board_api/board_api.py
@@ -1,12 +1,26 @@
 from flask import Flask
+from pymongo import MongoClient
+from bson import json_util
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello API'
+    return 'Hello API'\
 
+@app.route('/all')
+def all():
+    conn = 'mongodb://localhost:32768/?3t.uriVersion=2&3t.connectionMode=direct&readPreference=primary&3t.connection.name=localhost%3A32768'
+
+    cl = MongoClient(conn)
+    coll = cl["local"]["test2"]
+
+    data = [{"foo": "HELLO"}, {"Blah": "Bloh"}]
+    for d in data:
+        coll.insert_one(data)
+
+    return 'all'
 
 if __name__ == '__main__':
     app.run()
