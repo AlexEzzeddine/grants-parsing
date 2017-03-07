634be0ede25f82a490a464a1f391c8d438a0cb05
ohoncharuk <alex@honcharuk.me>
2017-03-07 18:13:02 +0200

    EB enabled

--- board_api/board_api.py
+++ board_api/board_api.py
@@ -1,33 +1,37 @@
-# -*- coding: utf-8 -*-
-
 from flask import Flask
-from flask import jsonify
 from flask import request
 from mongoengine import *
 
-app = Flask(__name__)
+# EB looks for an 'application' callable by default.
+application = Flask(__name__)
 
 connect('databoard', host='ec2-54-237-130-222.compute-1.amazonaws.com', port=27017, username="root", password="test12345", authentication_source="admin")
 
+
 class Grants(Document):
-	id = ObjectIdField(required=False)
-	url = StringField(required=False)
-	title = StringField(required=False)
-	text = StringField(required=False)
-	contacts = StringField(required=False)
-	itemType = StringField(required=False)
-	modified = BooleanField(required=False)
+    id = ObjectIdField(required=False)
+    url = StringField(required=False)
+    title = StringField(required=False)
+    text = StringField(required=False)
+    contacts = StringField(required=False)
+    itemType = StringField(required=False)
+    modified = BooleanField(required=False)
 
 
-@app.route('/')
+@application.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'Hello 1!'
 
-@app.route('/all')
+
+@application.route('/all')
 def get_all():
-	page=int(request.args['page'])
-	pageSize=int(request.args['pageSize'])
-	return Grants.objects.skip((page - 1) * pageSize).limit(pageSize).to_json().encode('utf-8')
+    page=int(request.args['page'])
+    pageSize=int(request.args['pageSize'])
+    return Grants.objects.skip((page - 1) * pageSize).limit(pageSize).to_json().encode('utf-8')
 
-if __name__ == '__main__':
-    app.run()
+# run the app.
+if __name__ == "__main__":
+    # Setting debug to True enables debug output. This line should be
+    # removed before deploying a production app.
+    application.debug = True
+    application.run()
