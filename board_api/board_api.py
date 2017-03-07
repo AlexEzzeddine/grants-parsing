8b469c2b67147b6a7711dc829aa85dc92a182905
Alex <alex.izzedin@gmail.com>
2017-03-07 13:22:09 +0200

    show all documents

--- board_api/board_api.py
+++ board_api/board_api.py
@@ -1,36 +1,30 @@
+# -*- coding: utf-8 -*-
+
 from flask import Flask
-from bson.json_util import dumps
+from flask import jsonify
 from mongoengine import *
 
 app = Flask(__name__)
 
-connect('databoard', host='localhost', port=32768)
+connect('databoard', host='ec2-54-237-130-222.compute-1.amazonaws.com', port=27017, username="root", password="test12345", authentication_source="admin")
 
 class Grants(Document):
-    url = StringField(required=False)
-    title = StringField(required=False)
-    text = StringField(required=False)
-    contacts = StringField(required=False)
-    itemType = StringField(required=False)
-    modified = BooleanField(required=False)
+	id = ObjectIdField(required=False)
+	url = StringField(required=False)
+	title = StringField(required=False)
+	text = StringField(required=False)
+	contacts = StringField(required=False)
+	itemType = StringField(required=False)
+	modified = BooleanField(required=False)
 
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
-
 @app.route('/all')
 def get_all():
-
-    return dumps(Grants.objects)
-
-    for u in Grants.objects:
-
-        return u.url;
-
-    return "OK"
-
+	return Grants.objects[:10].to_json().encode('utf-8')
 
 if __name__ == '__main__':
     app.run()
