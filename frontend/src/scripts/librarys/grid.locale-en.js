455524ae4f6677e7b8d143f1085390b0242808a3
BLuchenko <luchenko.bogdan@gmail.com>
2017-03-07 16:36:13 +0200

    example jqgrid

--- frontend/src/scripts/librarys/grid.locale-en.js
+++ frontend/src/scripts/librarys/grid.locale-en.js
@@ -1,7 +1,7 @@
 /**
  * jqGrid English Translation
  * Tony Tomov tony@trirand.com
- * http://trirand.com/blog/
+ * http://trirand.com/blog/ 
  * Dual licensed under the MIT and GPL licenses:
  * http://www.opensource.org/licenses/mit-license.php
  * http://www.gnu.org/licenses/gpl.html
