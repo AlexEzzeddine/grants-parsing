b6a528e09f30c29a72e9466f68432b199600b894
Alex <alex.izzedin@gmail.com>
2017-03-09 12:29:43 +0200

    formatting

--- board_api/application.py
+++ board_api/application.py
@@ -1,13 +1,12 @@
-from flask import Flask
-from flask import request
-from flask import jsonify
-from flask import Response
 from bson.json_util import dumps
-import json
+from flask import Flask
+from flask import Response
+from flask import request
+from flask_cors import CORS
 from mongoengine import *
-
 # EB looks for an 'application' callable by default.
 application = Flask(__name__)
+CORS(application)
 
 connect('databoard', host='ec2-54-237-130-222.compute-1.amazonaws.com', port=27017, username="root",
         password="test12345", authentication_source="admin")
@@ -31,10 +30,13 @@ def hello_world():
 @application.route('/grants')
 def get_all():
     page = int(request.args.get('page'))
-    page_size = int(request.args.get('pageSize'))
+    page_size = int(request.args.get('page_size'))
     return Response(dumps({
                             "Count": Grants.objects.count(),
-                            "Data" : [o.to_mongo() for o in Grants.objects.skip((page - 1) * page_size).limit(page_size)]
+                            "Data": [
+                                        o.to_mongo()
+                                        for o in Grants.objects.skip((page - 1) * page_size).limit(page_size)
+                                     ]
                           } , indent=2 , ensure_ascii=False) , mimetype='application/json')
 
 
@@ -42,6 +44,5 @@ def get_all():
 if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
     # removed before deploying a production app.
-    application.config['JSON_AS_ASCII'] = False
     application.debug = True
     application.run()
