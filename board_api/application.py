9d48263a266d69b4c7512d775c1d3346839f15c0
ohoncharuk <alex@honcharuk.me>
2017-03-09 11:31:56 +0200

    formatting

--- board_api/application.py
+++ board_api/application.py
@@ -29,9 +29,9 @@ def hello_world():
 @application.route('/grants')
 def get_all():
     page = int(request.args.get('page'))
-    pageSize = int(request.args.get('pageSize'))
+    page_size = int(request.args.get('pageSize'))
     return jsonify({"Count": Grants.objects.count(),
-                    "Data": json.loads(Grants.objects.skip((page - 1) * pageSize).limit(pageSize).to_json())})
+                    "Data": json.loads(Grants.objects.skip((page - 1) * page_size).limit(page_size).to_json())})
 
 
 # run the app.
