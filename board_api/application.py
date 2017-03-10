de851d5f9fa4e58a41bbbfdf7db1524323a09f9e
Alex <alex.izzedin@gmail.com>
2017-03-10 16:56:06 +0200

    added viewmodel

--- board_api/application.py
+++ board_api/application.py
@@ -12,15 +12,32 @@ CORS(application)
 connect('databoard', host='ec2-54-237-130-222.compute-1.amazonaws.com', port=27017, username="root",
         password="test12345", authentication_source="admin")
 
+class Flags(Document):
+    important=BooleanField(default=False)
+    displayed=BooleanField(default=False)
+    skipped=BooleanField(default=False)
+    done=BooleanField(default=False)
+
+class ViewModel:
+    id=""
+    url=""
+    title=""
+    text=""
+    contacts="N/A"
+    itemType=""
+    modified=False
+    publication_date=0
+    flags=Flags()
 
 class Grants(DynamicDocument):
     _id = ObjectIdField(required=False)
     url = StringField(required=False)
     title = StringField(required=False)
     text = StringField(required=False)
-    contacts = StringField(required=False)
+    contacts = StringField(required=False,default="N/A")
     itemType = StringField(required=False)
     modified = BooleanField(required=False)
+    publication_date = DateTimeField(required=False)
     flags = DynamicField(required=False, default={
         "important": False,
         "displayed": False,
@@ -38,14 +55,36 @@ def hello_world():
 
 @application.route('/grants')
 def get_all():
+    query = request.args.get('query')
     page = int(request.args.get('page'))
     page_size = int(request.args.get('page_size'))
+    important = bool(request.args.get("important", None))
+    displayed = bool(request.args.get("displayed", None))
+    skipped = bool(request.args.get("skipped", None))
+    done = bool(request.args.get("done", None))
+    data=[]
+    for grant in Grants.objects(__raw__={}).skip((page - 1) * page_size).limit(page_size):
+        item= {
+            "id":str(grant._id),
+            "url":grant.url,
+            "title":grant.title,
+            "text":grant.text,
+            "contacts":grant.contacts,
+            "itemType":grant.itemType,
+            "modified":grant.modified,
+            "publication_date":str(grant.publication_date),
+            "flags":{
+                "important" : grant.flags['important'],
+                "displayed": grant.flags['displayed'],
+                "skipped": grant.flags['skipped'],
+                "done": grant.flags['done']
+            }
+        }
+        data.append(item)
+
     return Response(dumps({
-        "Count": Grants.objects.count(),
-        "Data": [
-            o.to_mongo()
-            for o in Grants.objects.skip((page - 1) * page_size).limit(page_size)
-            ]
+        "Count": Grants.objects(__raw__=dict([flag for flag in [important,displayed,skipped,done] if flag==True])).count(),
+        "Data": data
     }, indent=2, ensure_ascii=False), mimetype='application/json')
 
 
