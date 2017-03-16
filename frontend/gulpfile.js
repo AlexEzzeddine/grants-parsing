d8ff18153870c42274c1ca841d41883eb8df32f1
max.shv <max.shvydkyi@gmail.com>
2017-03-16 14:09:12 +0200

    #GP gulp config

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -20,22 +20,22 @@ gulp.task('html', function () {
 gulp.task('sass', function () {
     return gulp.src('src/styles/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('src/styles/sass-css'))
+        .pipe(gulp.dest('.tmp/styles/sass-css'))
         .pipe(livereload());
 });
 
 gulp.task('preffix', function () {
-    gulp.src('src/styles/sass-css/*.css')
+    gulp.src('.tmp/styles/sass-css/*.css')
         .pipe(autoprefixer({
             browsers: ['last 2 versions'],
             cascade: false
         }))
-        .pipe(gulp.dest('src/styles/css-preffix'))
+        .pipe(gulp.dest('.tmp/styles/css-preffix'))
         .pipe(livereload());
 });
 
 gulp.task('minify-css', function () {
-    return gulp.src('src/styles/css-preffix/*.css')
+    return gulp.src('.tmp/styles/css-preffix/*.css')
         .pipe(cleanCSS({compatibility: 'ie8'}))
         .pipe(gulp.dest('dist/styles'))
         .pipe(livereload());
@@ -45,7 +45,7 @@ gulp.task('js-custom-min', function () {
     gulp.src('src/scripts/customs/*.js')
         .pipe(jsmin())
         .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('src/scripts/min/customs/'))
+        .pipe(gulp.dest('.tmp/scripts/min/customs/'))
         .pipe(livereload());
 });
 
@@ -53,26 +53,26 @@ gulp.task('js-library-min', function () {
     gulp.src('src/scripts/librarys/*.js')
         .pipe(jsmin())
         .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('src/scripts/min/librarys/'))
+        .pipe(gulp.dest('.tmp/scripts/min/librarys/'))
         .pipe(livereload());
 });
 
 gulp.task('js-concat-customs', function () {
-    return gulp.src('src/scripts/min/customs/*.js')
+    return gulp.src('.tmp/scripts/min/customs/*.js')
         .pipe(concat('all-customs.js'))
         .pipe(gulp.dest('dist/scripts/'))
         .pipe(livereload());
 });
 
 gulp.task('js-concat-librarys', function () {
-    return gulp.src('src/scripts/min/librarys/*.js')
+    return gulp.src('.tmp/scripts/min/librarys/*.js')
         .pipe(concat('all-librarys.js'))
         .pipe(gulp.dest('dist/scripts/'))
         .pipe(livereload());
 });
 
 gulp.task('css-libs-concat', function () {
-    return gulp.src('src/styles/librarys/*.css')
+    return gulp.src('.tmp/styles/librarys/*.css')
         .pipe(concatCss("librarys-concat.css"))
         .pipe(gulp.dest('dist/styles'))
         .pipe(livereload());
@@ -91,33 +91,25 @@ gulp.task('img-saver', function () {
         .pipe(livereload());
 });
 
-gulp.task('clean-sass-css', function () {
-    return gulp.src('src/styles/sass-css/*.css', {read: false})
-        .pipe(clean())
-        .pipe(livereload());
-});
-gulp.task('clean-css-preffix', function () {
-    return gulp.src('src/styles/css-preffix/*.css', {read: false})
-        .pipe(clean())
-        .pipe(livereload());
+gulp.task('clean', function () {
+    return gulp.src('.tmp', {read: false})
+        .pipe(clean());
 });
 
 
 gulp.task('watch', function () {
     var server = livereload.listen();
     gulp.watch('src/styles/*.scss', ['sass']);
-    gulp.watch('src/styles/sass-css/*.css', ['preffix']);
-    gulp.watch('src/styles/css-preffix/*.css', ['minify-css']);
+    gulp.watch('.tmp/styles/sass-css/*.css', ['preffix']);
+    gulp.watch('.tmp/styles/css-preffix/*.css', ['minify-css']);
     gulp.watch('*.html', ['html']);
     gulp.watch('src/scripts/customs/*.js', ['js-custom-min']);
     gulp.watch('src/scripts/librarys/*.js', ['js-library-min']);
-    gulp.watch('src/scripts/min/customs/*.js', ['js-concat-customs']);
-    gulp.watch('src/scripts/min/librarys/*.js', ['js-concat-librarys']);
-    gulp.watch('src/styles/librarys/*.css', ['css-libs-concat']);
+    gulp.watch('.tmp/scripts/min/customs/*.js', ['js-concat-customs']);
+    gulp.watch('.tmp/scripts/min/librarys/*.js', ['js-concat-librarys']);
+    gulp.watch('.tmp/styles/librarys/*.css', ['css-libs-concat']);
     gulp.watch('*.json', ['data-saver']);
     gulp.watch('*.json', ['img-saver']);
-    gulp.watch('src/styles/css-preffix/*.css', ['clean-sass-css']);
-    gulp.watch('dist/styles/*.css', ['clean-css-preffix']);
 });
 
-gulp.task('default', ['html', 'sass', 'preffix', 'minify-css', 'js-custom-min', 'js-custom-min', 'js-concat-customs', 'js-concat-librarys', 'css-libs-concat', 'data-saver', 'img-saver', 'watch', 'clean-sass-css', 'clean-css-preffix']);
+gulp.task('default', ['html', 'sass', 'preffix', 'minify-css', 'js-library-min', 'js-custom-min', 'js-concat-customs', 'js-concat-librarys', 'css-libs-concat', 'data-saver', 'img-saver', 'watch']);
