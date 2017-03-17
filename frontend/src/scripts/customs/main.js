6c170543d8b629fe2348b9d2e3f7b135d0c37fb2
BLuchenko <luchenko.bogdan@gmail.com>
2017-03-17 11:51:10 +0200

    data update

--- frontend/src/scripts/customs/main.js
+++ frontend/src/scripts/customs/main.js
@@ -1,8 +1,8 @@
 var grantId,
     categoriesStr = ":All;Grant:Grant;Conference:Сonference;****:****;***:***",
-    host = 'https://shielded-fortress-95039.herokuapp.com',
-    //localRoute = 'http://127.0.0.1:5000',
-    //host = localRoute,
+    //host = 'https://shielded-fortress-95039.herokuapp.com',
+    localRoute = 'http://127.0.0.1:5000',
+    host = localRoute,
     routeArguments = '/grants?page_size=20',
     routeData = host + "/last_updated_date";
 
@@ -10,7 +10,7 @@ var grantId,
 $(document).ready(function () {
 
     $.get(routeData, function (data) {
-        document.getElementById("last-updated-time").innerHTML = "Last updated on: " + data.date;
+        document.getElementById("last-updated-time").innerHTML = "Last updated on: " + data;
     });
 
 
