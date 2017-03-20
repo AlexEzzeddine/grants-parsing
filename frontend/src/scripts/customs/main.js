fcd4eb535d9a007441d61a1933f1935220bc31a1
Alex <alex.izzedin@gmail.com>
2017-03-20 23:06:46 +0200

    refactoring

--- frontend/src/scripts/customs/main.js
+++ frontend/src/scripts/customs/main.js
@@ -1,92 +1,128 @@
 var grantId,
     host = 'https://shielded-fortress-95039.herokuapp.com',
     //host = 'http://127.0.0.1:5000',
-    routeArguments = '/grants?',
+    grantsRoute = host + '/grants',
+    lastUpdatedDateRoute = host + "/last_updated_date",
+    domainsRoute = host + "/domains",
     pageSize = 16,
-    routeData = host + "/last_updated_date";
-var categoriesStr = ":All";
-$.get(host + "/domains", function (data) {
-    for (var i = 0; i < data.length; i++) {
-        categoriesStr += ";" + data[i] + ":" + data[i];
-    }
-});
+    domains = {
+        All: "All"
+    };
 
 
-$(document).ready(function () {
-
-    $.get(routeData, function (data) {
-        document.getElementById("last-updated-time").innerHTML = "Last updated on: " + data;
+function getDomains(){
+    $.ajax({
+        url: domainsRoute,
+        async: false,
+        success: function(data){
+            for (var i = 0; i < data.length; i++){
+                domains[data[i]] = data[i];
+            }
+        }
     });
+}
+
+function displayLastUpdatedDate(){
+    $.get(lastUpdatedDateRoute, function(data){
+        $("#last-updated-date").text("Last updated on: " + data);
+    });
+}
+
+$(document).ready(function(){
+
+    getDomains();
+    displayLastUpdatedDate();
 
     var grid = $("#jqGrid");
     grid.jqGrid({
-        url: host + routeArguments + "page_size=" + pageSize,
+        url: grantsRoute + "?page_size=" + pageSize,
         mtype: "GET",
         datatype: "json",
         jsonReader: {
             root: "Data",
             //page:  function(obj) { console.error("!!!", obj); return 1; },
-            total: function (obj) {
-                return obj.Count / 20;
+            total: function(obj){
+                return obj.Count / pageSize;
             },
-            records: function (obj) {
-                return obj.Count;
+            records: "Count"
             },
-        },
-        colModel: [
-            {label: 'Status', name: 'Status', width: 50, formatter: statusStyles, sortable: false, search: false},
-            {
-                label: 'Date',
-                name: 'publication_date',
-                width: 75,
-                formatter: dateStyles,
-                sortable: false,
-                search: false
-            },
-            {
-                label: 'Source',
-                name: 'domain',
-                width: 100,
-                formatter: typeStyles,
-                stype: 'select',
-                searchoptions: {value: categoriesStr}
-            },
-            {label: 'Title', name: 'url', formatter: linkStyles, width: 210, sortable: false, search: false},
-            {
-                label: 'Content preview',
-                name: 'text',
-                width: 580,
-                formatter: prewievStyles,
-                sortable: false,
-                search: false
-            },
-            {
-                name: 'title',
-                width: 0,
-                formatter: prewievStyles,
-                sortable: false,
-                search: false,
-                hidden: true
-            },
-            {
-                name: '_id',
-                width: 0,
-                sortable: false,
-                search: false,
-                hidden: true
-            },
-            {
-                label: 'Contacts',
-                name: 'contacts',
-                width: 280,
-                formatter: contactsStyles,
-                sortable: false,
-                search: false
-            },
-            {label: 'Actions', width: 250, formatter: actionsButtons, sortable: false, search: false}
-        ],
-        viewrecords: true, // show the current page, data rang and total records on the toolbar
-        height: 750,
+            colModel: [
+                {
+                    label: 'Status',
+                    name: 'Status',
+                    width: 50,
+                    formatter: statusStyles,
+                    sortable: false,
+                    search: false
+                },
+                {
+                    label: 'Date',
+                    name: 'publication_date',
+                    width: 75,
+                    formatter: dateStyles,
+                    sortable: false,
+                    search: false
+                },
+                {
+                    label: 'Source',
+                    name: 'domain',
+                    width: 100,
+                    formatter: typeStyles,
+                    stype: 'select',
+                    searchoptions: {
+                        value: domains
+                    }
+                },
+                {
+                    label: 'Title',
+                    name: 'url',
+                    formatter: linkStyles,
+                    width: 210,
+                    sortable: false,
+                    search: false
+                },
+                {
+                    label: 'Content preview',
+                    name: 'text',
+                    width: 580,
+                    formatter: previewStyles,
+                    sortable: false,
+                    search: false
+
+                },
+                {
+                    name: 'title',
+                    width: 0,
+                    formatter: previewStyles,
+                    sortable: false,
+                    search: false,
+                    hidden: true
+                },
+                {
+                    name: '_id',
+                    width: 0,
+                    sortable: false,
+                    search: false,
+                    hidden: true
+                },
+                {
+                    label: 'Contacts',
+                    name: 'contacts',
+                    width: 280,
+                    formatter: contactsStyles,
+                    sortable: false,
+                    search: false
+                },
+                {
+                    label: 'Actions',
+                    width: 250,
+                    formatter: actionsButtons,
+                    sortable: false,
+                    search: false
+                }],
+
+                viewrecords: true, // show the current page, data rang and total records on the toolbar
+   height: 750,
         hidegrid: false,
         autowidth: true,
         rowheight: 20,
@@ -103,35 +139,35 @@ $(document).ready(function () {
     grid.jqGrid('navButtonAdd', '#' + grid[0].id + '_toppager_left', {
         caption: "<div class='add_record_button'><i class='fa fa-envelope-o' aria-hidden='true'></i>UNREAD</div>",
         buttonicon: 'none',
-        onClickButton: function () {
+        onClickButton: function() {
             alert("UNREAD");
         }
     });
     grid.jqGrid('navButtonAdd', '#' + grid[0].id + '_toppager_left', {
         caption: "<div class='add_record_button'><i class='fa fa-exclamation-circle fa-lg' aria-hidden='true'></i>IMPORTANT</div>",
         buttonicon: 'none',
-        onClickButton: function () {
+        onClickButton: function() {
             alert("IMPORTANT");
         }
     });
     grid.jqGrid('navButtonAdd', '#' + grid[0].id + '_toppager_left', {
         caption: "<div class='add_record_button'><i class='fa fa-exclamation-circle fa-lg' aria-hidden='true'></i>SKIPPED</div>",
         buttonicon: 'none',
-        onClickButton: function () {
+        onClickButton: function() {
             alert("SKIPPED");
         }
     });
     grid.jqGrid('navButtonAdd', '#' + grid[0].id + '_toppager_left', {
         caption: "<div class='add_record_button'><i class='fa fa-check-circle-o fa-lg' aria-hidden='true'></i> DONE</div>",
         buttonicon: 'none',
-        onClickButton: function () {
+        onClickButton: function() {
             alert("DONE");
         }
     });
     grid.jqGrid('navButtonAdd', '#' + grid[0].id + '_toppager_left', {
         caption: "<div class='add_record_button'><i class='fa fa-pencil' aria-hidden='true'></i>MODIFIED</div>",
         buttonicon: 'none',
-        onClickButton: function () {
+        onClickButton: function() {
             alert("MODIFIED");
         }
     });
@@ -140,26 +176,27 @@ $(document).ready(function () {
 
 function statusStyles(cellValue, options, rowObject) {
     var statusIcon = 'fa-envelope-o';
-    if(rowObject.flags.displayed){
+    if (rowObject.flags.displayed) {
         statusIcon = 'fa-desktop';
     }
-    if(rowObject.flags.done){
+    if (rowObject.flags.done) {
         statusIcon = 'fa-check-circle-o';
     }
-    if(rowObject.flags.important){
+    if (rowObject.flags.important) {
         statusIcon = 'fa-exclamation-circle';
     }
-    if(rowObject.flags.skipped){
+    if (rowObject.flags.skipped) {
         statusIcon = 'fa-ban';
     }
-    if(rowObject.flags.modified){
+    if (rowObject.flags.modified) {
         statusIcon = 'fa-pencil';
     }
 
-    return '<div class="statusStyles">'+         
-        '<i class="fa '+statusIcon+'" aria-hidden="true"></i>'+
+    return '<div class="statusStyles">' +
+        '<i class="fa ' + statusIcon + '" aria-hidden="true"></i>' +
         '</div>';
 }
+
 function dateStyles(v) {
     return '<div class=" dateStyles">' + v + '</div>';
 }
@@ -171,6 +208,7 @@ function typeStyles(v) {
 function linkStyles(cellValue, options, rowObject) {
     return "<a class='linkStyles' onclick=\"clickedLink($(this).closest('tr'))\" style='color: #3f51b5'  target='_blank' href='\" + cellValue + \"'+>" + rowObject.title + "</a>";
 }
+
 function clickedLink(e) {
     e.addClass('displayed');
 
@@ -179,16 +217,17 @@ function clickedLink(e) {
     setGridItemStatus(data._id, 'displayed', id);
 }
 
-function prewievStyles(v) {
-    return '<div class="prewievStyles">' + v + '</div>';
+function previewStyles(v) {
+    return '<div class="previewStyles">' + v + '</div>';
 }
+
 function contactsStyles(v) {
     return '<div class="contactsStyles">' + v + '</p></div>';
 }
 
 function actionsButtons(cellValue, options, rowObject) {
     if (rowObject.flags.displayed || rowObject.flags.done || rowObject.flags.important || rowObject.flags.skipped) {
-        setTimeout(function () {
+        setTimeout(function() {
             $("#" + options.rowId).addClass('displayed');
 
         }, 50);
@@ -207,10 +246,10 @@ function displayItem(e) {
     var id = $(e).attr('id'),
         data = $("#jqGrid").getRowData(id);
 
-    document.getElementById('allertDate').innerHTML = "Date: " + $(data.publication_date).text();
-    document.getElementById('allertTitle').innerHTML = ($(data.title).text());
-    document.getElementById('allertContent').innerHTML = ($(data.text).text());
-    document.getElementById('allertContacts').innerHTML = $(data.contacts).text();
+    document.getElementById('alertDate').innerHTML = "Date: " + $(data.publication_date).text();
+    document.getElementById('alertTitle').innerHTML = ($(data.title).text());
+    document.getElementById('alertContent').innerHTML = ($(data.text).text());
+    document.getElementById('alertContacts').innerHTML = $(data.contacts).text();
     document.getElementById('myModal').style.display = "flex";
     document.getElementById('body').style.overflow = "hidden";
 
@@ -223,9 +262,11 @@ function skip() {
     document.getElementsByClassName("dateStyles").style.fontWeight = "normal";
     setGridItemStatus(grantId, 'skipped', null);
 }
+
 function importAnt() {
     setGridItemStatus(grantId, 'important', null);
 }
+
 function donE() {
     setGridItemStatus(grantId, 'done', null);
 }
@@ -237,12 +278,14 @@ function skipItem(e) {
 
     setGridItemStatus(data._id, 'skipped', id);
 }
+
 function importantItem(e) {
     var id = $(e).attr('id'),
         data = $("#jqGrid").getRowData(id);
 
     setGridItemStatus(data._id, 'important', id);
 }
+
 function doneItem(e) {
     var id = $(e).attr('id'),
         data = $("#jqGrid").getRowData(id);
@@ -250,8 +293,8 @@ function doneItem(e) {
     setGridItemStatus(data._id, 'done', id);
 }
 
-var setGridItemStatus = function(grant_id, statusName, rowId){
-    if(!grant_id) return;
+var setGridItemStatus = function(grant_id, statusName, rowId) {
+    if (!grant_id) return;
 
     $.ajax({
         "url": host + "/status/" + grant_id,
@@ -260,30 +303,32 @@ var setGridItemStatus = function(grant_id, statusName, rowId){
             "status_name": statusName,
             "value": "true"
         },
-        success: function(res){
-            if(rowId) {
+        success: function(res) {
+            if (rowId) {
                 var newClass;
 
-                if(res.displayed) newClass = 'fa fa-desktop';
-                if(res.done) newClass = 'fa fa-check-circle-o';
-                if(res.important) newClass = 'fa fa-exclamation-circle';
-                if(res.modified) newClass = 'fa fa-pencil';
-                if(res.skipped) newClass = 'fa fa-ban';
+                if (res.displayed) newClass = 'fa fa-desktop';
+                if (res.done) newClass = 'fa fa-check-circle-o';
+                if (res.important) newClass = 'fa fa-exclamation-circle';
+                if (res.modified) newClass = 'fa fa-pencil';
+                if (res.skipped) newClass = 'fa fa-ban';
 
-                $('tr#'+rowId+' .statusStyles i').attr('class', newClass);
+                $('tr#' + rowId + ' .statusStyles i').attr('class', newClass);
             }
         },
-        error: function(e){
+        error: function(e) {
             console.error(e.statusText);
         }
     });
 };
 
-function closeAllert() {
-    $('#allertContent').animate({scrollTop: $('html').offset().top});
+function closeAlert() {
+    $('#alertContent').animate({
+        scrollTop: $('html').offset().top
+    });
     document.getElementById('myModal').style.display = "none";
 }
-window.onclick = function (event) {
+window.onclick = function(event) {
     if (event.target == document.getElementById('myModal')) {
         document.getElementById('myModal').style.display = "none";
     }
